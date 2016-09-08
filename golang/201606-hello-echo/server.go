ff098614517dc14ca4ecdbc5c27d30894210074b
bskang <bskang@bskang-n3n.local>
2016-09-08 16:45:57 +0900

    지난 노트 저장

---
+++ golang/201606-hello-echo/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "net/http"
+    
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/engine/standard"
+    "github.com/labstack/echo/middleware"
+)
+
+func main()  {
+    // Echo instance
+    e := echo.New()
+
+    // Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, World!\n")
+    })
+
+    // Start server
+    e.Run(standard.New(":1323"))
+}
